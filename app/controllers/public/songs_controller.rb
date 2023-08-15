928dd3df4344d913da31035d8ce8d356102b34fb
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-08-15 02:23:26 +0000

    [製作途中]user

--- app/controllers/public/songs_controller.rb
+++ app/controllers/public/songs_controller.rb
@@ -11,19 +11,41 @@ class Public::SongsController < ApplicationController
     if @song.save
       redirect_to user_path(current_user), notice: "You have created book successfully."
     else
+      @songs = Song.all
       @song = Song.new
       render :new
     end
   end
 
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
+    @user = @song.user
   end
 
   def edit
+    @song = Song.find(params[:id])
+    @genres = Genre.all
   end
+  
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path(@song), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to user_path(current_user)
+  end
+  
   
   private
 
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,20 @@
 class Public::UsersController < ApplicationController
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @songs = @user.songs.all
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(song_params)
+      redirect_to user_path(current_user), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def check
