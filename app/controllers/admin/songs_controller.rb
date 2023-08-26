6d802ff0b813fc952bb21415f678a209c6a572f8
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-08-26 18:12:03 +0000

    [製作途中]admin,public/song/index

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -1,9 +1,9 @@
 class Admin::SongsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @q = Song.ransack(params[:q])
-    @songs = @q.result(distinct: true).includes(:user)
+    @songs = @q.result(distinct: true).includes(:user).order(created_at: :desc)
   end
 
   def show
@@ -12,17 +12,17 @@ class Admin::SongsController < ApplicationController
     @comment = Comment.new
     @song_comments = @song.comments
   end
-  
+
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
     redirect_to admin_songs_path
   end
-  
+
   private
 
   def song_params
     params.require(:song).permit(:title, :body, :genre_id, :user_id)
   end
-  
+
 end
--- app/controllers/public/songs_controller.rb
+++ app/controllers/public/songs_controller.rb
@@ -20,7 +20,7 @@ class Public::SongsController < ApplicationController
 
   def index
     @q = Song.ransack(params[:q])
-    @songs = @q.result(distinct: true).includes(:user)
+    @songs = @q.result(distinct: true).includes(:user).order(created_at: :desc)
   end
 
   def show
@@ -34,7 +34,7 @@ class Public::SongsController < ApplicationController
     @song = Song.find(params[:id])
     @genres = Genre.all
   end
-  
+
   def update
     @song = Song.find(params[:id])
     if @song.update(song_params)
@@ -43,19 +43,19 @@ class Public::SongsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
     redirect_to user_path(current_user)
   end
-  
-  
+
+
   private
 
   def song_params
     params.require(:song).permit(:title,:body,:genre_id,:user_id)
-  end  
-  
+  end
+
 
 end
